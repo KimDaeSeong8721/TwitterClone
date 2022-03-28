266eeeadcc8cdf351ee7b01b356530f520048544
KimDaeSeong <KimDaeSeong8721@github.com>
2022-03-28 19:22:47 +0900

    Initial Commit

---
+++ TwitterSwiftUIYoutube/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by DaeSeong on 2022/03/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUIYoutube/TwitterSwiftUIYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIYoutubeApp.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by DaeSeong on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
